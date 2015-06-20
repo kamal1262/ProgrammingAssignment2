429f031e3d2240ef1163b95049b480c823bb6d02
kamal1262 <kamal1262@users.noreply.github.com>
2015-06-21 01:01:01 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,12 @@
+## Md. Kamal Hossain
+## M.Eng.Sc student
+## Multimedia University, Malaysia
+##email: md.kamal.hossain@hotmail.com
+
+##Assignment 2, coursera-online course on R Programming
+##source: https://class.coursera.org/rprog-015/human_grading/view/courses/973496/assessments/3/submissions 
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
